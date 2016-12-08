34fe99b1f4791f6d59a874d3bc9638bd58073251
Ben Blum <bblum@andrew.cmu.edu>
2016-12-08 18:34:38 -0500

    make day7hs a bit more concise

--- day7/Day7.hs
+++ day7/Day7.hs
@@ -1,8 +1,7 @@
 import Data.List.Utils (hasAny)
 import Data.Tuple (swap)
-import Control.Arrow ((***))
+import Control.Arrow ((***),(&&&),first)
 import Control.Monad (join)
-import Control.Applicative (liftA)
 
 abba (a:rest@(b:c:d:_)) = (a == d && b == c && a /= b) || abba rest
 abba _ = False
@@ -11,8 +10,8 @@ aba r (a:rest@(b:c:_)) = if (a == c && a /= b) then (a,b):(aba r rest) else aba 
 aba r _ = r
 
 supportsTLS = uncurry ((. not) . (&&)) . join (***) (any abba)
-supportsSSL = uncurry hasAny . liftA (map swap) . join (***) (concat . map (aba []))
+supportsSSL = uncurry hasAny . first (map swap) . join (***) (concat . map (aba []))
 
 parse c (x:xs,ys) = if c == '[' || c == ']' then ("":ys, x:xs) else ((c:x):xs, ys)
 
-main = interact $ (++"\n") . show . (length . filter supportsTLS *** length . filter supportsSSL) . join (,) . map (foldr parse ([""],[])) . lines
+main = interact $ (++"\n") . show . (length . filter supportsTLS &&& length . filter supportsSSL) . map (foldr parse ([""],[])) . lines
