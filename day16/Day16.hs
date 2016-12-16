ae9fcac7a4b8f17ff9c698540e2f27c6c646c90e
Ben Blum <bblum@andrew.cmu.edu>
2016-12-16 01:54:51 -0500

    Day 16, ezpz

---
+++ day16/Day16.hs
@@ -0,0 +1,11 @@
+import Data.List
+import Data.List.Extra
+import Data.Char
+
+dragon x = x ++ [False] ++ (map not $ reverse x)
+
+stuff n = take n $ until ((>= n) . length) dragon $ map (== '1') "10001110011110000"
+
+checksum = map (foldr1 (==)) . chunksOf 2
+
+main = print $ map (intToDigit . fromEnum) $ until (odd . length) checksum $ stuff 35651584 -- 272
