45f50aa8b09a0b0c7d3f141a3a6120e729265995
Ben Blum <bblum@andrew.cmu.edu>
2016-12-24 16:33:07 -0500

    make it better

--- day24/Day24.hs
+++ day24/Day24.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
-import Data.List
+import Data.Set (singleton, member, insert)
+import Data.List hiding (insert)
 import Data.Maybe
 import Control.Monad.State
 import Control.Arrow
@@ -9,13 +10,13 @@ graph input = ([ ([a,b], distance (xys !! a) (xys !! b)) | a <- ns, b <- ns, a <
           coord c = do y <- findIndex (any (== c)) input
                        x <- findIndex (== c) $ input !! y
                        return (read [c], (x,y))
-          distance start goal = evalState (bfs 0 goal [start]) [start]
-          bfs n goal ps = if any (== goal) ps then return n
+          distance start goal = evalState (bfs 0 goal [start]) $ singleton start
+          bfs n goal ps = if elem goal ps then return n
                           else bfs (n+1) goal =<< concat <$> mapM fnbrs ps
           fnbrs (x0,y0) = catMaybes <$> mapM try [(x0-1,y0),(x0+1,y0),(x0,y0-1),(x0,y0+1)]
           try pos@(x,y) = do visited <- get
-                             if input !! y !! x == '#' || elem pos visited then return Nothing
-                             else modify (pos:) >> return (Just pos)
+                             if input !! y !! x == '#' || member pos visited then return Nothing
+                             else modify (insert pos) >> return (Just pos)
 
 solve endpath (g,n) = minimum $ map (cost . endpath . (0:)) $ permutations [1..n]
     where cost p = sum $ map (fromJust . flip lookup g) $ zipWith (\a b -> sort [a,b]) p $ tail p
