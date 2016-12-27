e76d2879e911f376987a47e701fae8b77f217661
Ben Blum <bblum@andrew.cmu.edu>
2016-12-27 18:15:01 -0500

    oic

--- 2015/Day07.hs
+++ 2015/Day07.hs
@@ -1,18 +1,18 @@
-import Data.Map (empty, (!), insert)
+import Data.Map (empty, (!), insert, findWithDefault)
 import Data.List hiding (insert)
 import Data.Word
 import Data.Bits
 import Data.Char
 
-value m x = if isDigit $ head x then read x :: Word16 else m ! x
+value x = findWithDefault (read x :: Word16) x
 
-wire m [x,"RSHIFT",y,"->",z] = insert z $ shiftR (value m x) (read y)
-wire m [x,"LSHIFT",y,"->",z] = insert z $ shiftL (value m x) (read y)
-wire m [x,   "AND",y,"->",z] = insert z $ value m x .&. value m y
-wire m [x,    "OR",y,"->",z] = insert z $ value m x .|. value m y
-wire m [     "NOT",y,"->",z] = insert z $ complement $ value m y
-wire m [           y,"->",z] = insert z $ value m y
+wire m [x,"RSHIFT",y,"->",z] = insert z $ shiftR (value x m) (read y)
+wire m [x,"LSHIFT",y,"->",z] = insert z $ shiftL (value x m) (read y)
+wire m [x,   "AND",y,"->",z] = insert z $ value x m .&. value y m
+wire m [x,    "OR",y,"->",z] = insert z $ value x m .|. value y m
+wire m [     "NOT",y,"->",z] = insert z $ complement $ value y m
+wire m [           y,"->",z] = insert z $ value y m
 
-result input = foldr (wire $ result input) empty input
+result input = m where m = foldr (wire m) empty input
 
 main = interact $ (++"\n") . show . (! "a") . result . map words . lines
