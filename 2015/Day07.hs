196545de8c8cc5f13045bcd3984a63526031e14f
Ben Blum <bblum@andrew.cmu.edu>
2016-12-27 18:22:25 -0500

    golf

--- 2015/Day07.hs
+++ 2015/Day07.hs
@@ -3,16 +3,17 @@ import Data.List hiding (insert)
 import Data.Word
 import Data.Bits
 import Data.Char
+import Control.Monad
 
 value x = findWithDefault (read x :: Word16) x
 
-wire m [x,"RSHIFT",y,"->",z] = insert z $ shiftR (value x m) (read y)
-wire m [x,"LSHIFT",y,"->",z] = insert z $ shiftL (value x m) (read y)
-wire m [x,   "AND",y,"->",z] = insert z $ value x m .&. value y m
-wire m [x,    "OR",y,"->",z] = insert z $ value x m .|. value y m
-wire m [     "NOT",y,"->",z] = insert z $ complement $ value y m
-wire m [           y,"->",z] = insert z $ value y m
+wire m [x,"RSHIFT",y,"->",z] = shiftR (value x m) (read y)
+wire m [x,"LSHIFT",y,"->",z] = shiftL (value x m) (read y)
+wire m [x,   "AND",y,"->",z] = value x m .&. value y m
+wire m [x,    "OR",y,"->",z] = value x m .|. value y m
+wire m [     "NOT",y,"->",z] = complement $ value y m
+wire m [           y,"->",z] = value y m
 
-result input = m where m = foldr (wire m) empty input
+result input = m where m = foldr (liftM2 insert last $ wire m) empty input
 
 main = interact $ (++"\n") . show . (! "a") . result . map words . lines
