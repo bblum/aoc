b67a8a84c595e4c4e783526ab383b7b3aec88579
Ben Blum <bblum@andrew.cmu.edu>
2017-12-14 18:37:02 -0500

    clean up a bit

--- day14/Day14.hs
+++ day14/Day14.hs
@@ -5,19 +5,16 @@ import Data.Bits
 import Control.Monad
 import Numeric
 
-rot n list = drop n list ++ take n list
-chunk n list = if length list <= n then [list] else take n list : chunk n (drop n list)
-
 move ((pos,skip),list) len = (((pos + len + skip) `mod` length list, skip + 1), newlist)
-    where view = (reverse $ take len $ rot pos list) ++ (drop len $ rot pos list)
+    where rot n list = drop n list ++ take n list
+          view = (reverse $ take len $ rot pos list) ++ (drop len $ rot pos list)
           newlist = rot (length list - pos) view
 
-hash inp 0 state = map (foldl xor 0) $ chunk 16 $ snd state
-hash inp n state = hash inp (n-1) $ foldl move state $ inp ++ [17, 31, 73, 47, 23]
-
-hexify x = reverse $ take 8 $ (reverse $ showIntAtBase 2 intToDigit x "") ++ repeat '0'
-
 knothash inp = concat $ map hexify $ hash inp 64 ((0,0), [0..255] :: [Int])
+    where hexify x = reverse $ take 8 $ (reverse $ showIntAtBase 2 intToDigit x "") ++ repeat '0'
+          chunk n list = if length list <= n then [list] else take n list : chunk n (drop n list)
+          hash inp 0 state = map (foldl xor 0) $ chunk 16 $ snd state
+          hash inp n state = hash inp (n-1) $ foldl move state $ inp ++ [17, 31, 73, 47, 23]
 
 grid = map (map (=='1') . knothash . map fromEnum . ("vbqugkhl-" ++) . show) [0..127]
 
