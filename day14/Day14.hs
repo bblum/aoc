e8d6ea7023a9e3845250fbcc39d2674bb39e3aa3
Ben Blum <bblum@andrew.cmu.edu>
2017-12-14 18:16:26 -0500

    golf

--- day14/Day14.hs
+++ day14/Day14.hs
@@ -1,11 +1,9 @@
 {-# LANGUAGE TupleSections #-}
-import qualified Data.Set as S
 import Data.List
-import Data.Maybe
 import Data.Char
 import Data.Bits
+import Control.Monad
 import Numeric
-import Debug.Trace
 
 rot n list = drop n list ++ take n list
 chunk n list = if length list <= n then [list] else take n list : chunk n (drop n list)
@@ -22,15 +20,6 @@ hexify x = reverse $ take 8 $ (reverse $ showIntAtBase 2 intToDigit x "") ++ rep
 knothash inp = concat $ map hexify $ hash inp 64 ((0,0), [0..255] :: [Int])
 
 grid = map (map (=='1') . knothash . map fromEnum . ("vbqugkhl-" ++) . show) [0..127]
-
--- coords = foldr row S.empty $ zip grid [0..]
---     where row (r,i) s = foldr col s $ zip r [0..]
---               where col (v,j) s = if v then S.insert (i,j) s else s
--- 
--- bfs coords (i,j) = if S.member (i,j) coords then foldl bfs (S.delete (i,j) coords) nbrs else coords
---     where nbrs = [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
--- 
--- region coords = bfs coords $ S.elemAt 0 coords
 
 coords = concatMap row $ zip grid [0..]
     where row (r,i) = map snd $ filter fst $ zip r $ map (i,) [0..]
