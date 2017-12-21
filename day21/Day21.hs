274e8d516309acd1ae6af2a843afbee9634e93e6
Ben Blum <bblum@andrew.cmu.edu>
2017-12-21 01:24:07 -0500

    golf21

--- day21/Day21.hs
+++ day21/Day21.hs
@@ -1,38 +1,19 @@
 import Data.List
 import Data.List.Split
+import Control.Monad
+import Control.Arrow
 import Data.Maybe
 
-glider = [".#.","..#","###"]
+rotations x = foldr (liftM2 ($)) [x] $ map (:[id]) [reverse, map reverse, transpose]
 
-rotations noob = noobs ++ map transpose noobs
-    where noobs = [noob, reverse noob, map reverse noob, map reverse $ reverse noob]
+zoom n = map (transpose . map (chunksOf n)) . chunksOf n
 
-findrule rules noob = head $ catMaybes $ map (flip lookup rules) $ rotations noob
+assemble n = concatMap (\xs -> map (flip concatMap xs . flip (!!)) [0..n])
 
-breakline2 [] [] = []
-breakline2 (a1:a2:a) (b1:b2:b) = [[a1,a2],[b1,b2]] : breakline2 a b
-break2 [] = []
-break2 (a:b:rest) = breakline2 a b : break2 rest
+solve i rules = sum $ map (length . filter (=='#')) $ iterate step [".#.","..#","###"] !! i
+    where findrule = head . catMaybes . map (flip lookup rules) . rotations
+          step x = assemble n $ map (map findrule) $ zoom n x
+              where n = 2 + mod (length x) 2
 
-breakline3 [] [] [] = []
-breakline3 (a1:a2:a3:a) (b1:b2:b3:b) (c1:c2:c3:c) = [[a1,a2,a3],[b1,b2,b3],[c1,c2,c3]] : breakline3 a b c
-break3 [] = []
-break3 (a:b:c:rest) = breakline3 a b c : break3 rest
-
-assemblerow3 noobs = map (flip concatMap noobs . flip (!!)) [0..2]
-assemble3 grid = concatMap assemblerow3 grid
-
-assemblerow4 noobs = map (flip concatMap noobs . flip (!!)) [0..3]
-assemble4 grid = concatMap assemblerow4 grid
-
-step rules noob =
-    if length (head noob) `mod` 2 == 0 then
-        assemble3 $ map (map $ findrule rules) $ break2 noob
-    else
-        assemble4 $ map (map $ findrule rules) $ break3 noob
-
-solve rules n = sum $ map (length . filter (=='#')) $ iterate (step rules) glider !! n
-
-main = do input <- map ((\[x,y]->(x,y)) . map (splitOn "/") . words) <$> lines <$> readFile "input.txt"
-          print $ solve input 5
-          print $ solve input 18
+main = interact $ show . (solve 5 &&& solve 18)
+                  . map ((\[x,_,y]->(x,y)) . map (splitOn "/") . words) . lines
