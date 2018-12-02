1a33118bf1acddbbb8c64931fcf9cec897fdc812
Ben Blum <bblum@andrew.cmu.edu>
2018-12-02 10:54:20 -0500

    golf 02 more

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -1,11 +1,10 @@
+{-# LANGUAGE MonadComprehensions #-}
 import Data.List
 import Data.Maybe
 
 count = (length .) . filter
 anyn n str = any (\l -> count (== l) str == n) ['a'..'z']
 
-same x y = if x == y then Just x else Nothing
-
 main = do input <- lines <$> readFile "input.txt"
           print (count (anyn 2) input * count (anyn 3) input)
-          print [ z | x <- input, y <- input, let z = catMaybes $ zipWith same x y, length (x \\ z) == 1]
+          print [ z | x <- input, y <- input, let z = catMaybes $ zipWith (\x y -> [x|x==y]) x y, length (x \\ z) == 1]
