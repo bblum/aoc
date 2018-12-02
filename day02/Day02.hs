b7736481b748c304dd5e8b6d5318092969cf6d8d
Ben Blum <bblum@andrew.cmu.edu>
2018-12-02 11:28:10 -0500

    golf 02 more

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE MonadComprehensions #-}
 import Data.List
 import Data.Maybe
+import Control.Arrow
 
 count = (length .) . filter
 anyn n str = any (\l -> count (== l) str == n) ['a'..'z']
 
-main = do input <- lines <$> readFile "input.txt"
-          print (count (anyn 2) input * count (anyn 3) input)
-          print [ z | x <- input, y <- input, let z = catMaybes $ zipWith (\x y -> [x|x==y]) x y, length (x \\ z) == 1]
+part1 input = count (anyn 2) input * count (anyn 3) input
+part2 input = [ z | x <- input, y <- input, let z = catMaybes $ zipWith (\x y -> [x|x==y]) x y, length (x \\ z) == 1]
+
+main = interact $ (++"\n") . show . (part1 &&& part2) . lines
