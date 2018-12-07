981e88de9ed3684a106e6b946fa075bb9a60b9e0
Ben Blum <bblum@andrew.cmu.edu>
2018-12-07 02:20:52 -0500

    golf d7, i don't really care if it's not nice, eh

--- day07/Day07.hs
+++ day07/Day07.hs
@@ -1,63 +1,20 @@
-{-# LANGUAGE FlexibleContexts, TupleSections, MonadComprehensions #-}
-import qualified Data.Map as M
-import qualified Data.Set as S
 import Data.List
-import Data.Maybe
-import Data.Char
-import Data.Ord
-import Control.Monad.State
 import Control.Arrow
-import Debug.Trace
 
-free1 c s = all (\(_,c') -> c /= c') s
-findstep1 s done = find (\c -> free1 c s) ("ABCDEFGHIJKLMNOPQRSTUVWXYZ" \\ done)
+elf n [] = ([],[])
+elf 0 x = ([],x)
+elf n ((a,1):rest) = first (a:) (elf (n-1) rest)
+elf n ((a,t):rest) = second ((a,t-1):) (elf (n-1) rest)
 
-makestep1 done s = traceShow s $
-    let c = findstep1 s done
-    in case c of
-           Just c -> c:(makestep1 (c:done) $ filter (\(c',_) -> c' /= c) s)
-           Nothing -> []
+solve n t todo work s = step [ c | length work < n, c <- take 1 $ todo \\ (map fst work ++ map snd s) ]
+    where step [c] = solve n t (todo \\ [c]) ((c, 61 + fromEnum c - fromEnum 'A'):work) s
+          step [] = let (done,work') = elf n work
+                    in (done ++) <$> (if null work then (t,[]) else solve n (t+1) todo work' $ filter (not . flip elem done . fst) s)
 
+solve' n = solve n 0 "ABCDEFGHIJKLMNOPQRSTUVWXYZ" [] input
 
-free c s = all (\(_,c') -> c /= c') s
-notpending pending c = all (\(c',_) -> c /= c') pending
-findstep s done pending =
-    --let ans =
-    find (\c -> free c s && notpending pending c) ("ABCDEFGHIJKLMNOPQRSTUVWXYZ" \\ done)
-    --in traceShow (done,s,pending,ans) ans
+main = print (snd $ solve' 1, fst $ solve' 5)
 
-time c = 61 + fromEnum c - fromEnum 'A'
--- time c = 1 + fromEnum c - fromEnum 'A'
-
-incr :: Int -> [(Char,Int)] -> ([Char],[(Char,Int)])
-incr n [] = ([],[])
-incr 0 x = ([],x)
-incr n ((a,1):rest) =
-    let (finished,result) = (incr (n-1) rest) in (a:finished,result)
-incr n ((a,at):rest) =
-    let (finished,result) = (incr (n-1) rest) in (finished,(a,at-1):result)
-
-
-numworkers = 5
-makestep timestamp done pending s = traceShow (timestamp,pending) $
-    let c = if (length pending == numworkers) then Nothing else findstep s done pending
-    in case c of
-           Just c -> makestep (timestamp) (c:done) (sortBy (comparing fst) ((c,time c):pending)) s
-           Nothing -> let (finished,newpending) = incr numworkers pending
-                          news = filter (\(c',_) -> not $ elem c' finished) s
-                      in finished ++ (if null pending then [] else makestep (timestamp+1) done (newpending) news)
-
-main = print (makestep1 [] input, makestep 0 [] [] input)
-
-
-testinput = [
-    ('C','A'),
-    ('C','F'),
-    ('A','B'),
-    ('A','D'),
-    ('B','E'),
-    ('D','E'),
-    ('F','E')]
 
 input = [
     ('E','Y'),
