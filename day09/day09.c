9f43098f336652a1640b2aa738487a8d436d02ea
Ben Blum <bblum@andrew.cmu.edu>
2018-12-09 11:07:11 -0500

    9

--- day09/day09.c
+++ day09/day09.c
@@ -5,18 +5,18 @@
 struct nobe { long val; struct nobe *prev, *next; };
 
 void solve(long elves, long marbs) {
-	struct nobe zero = { .val = 0, .next = &zero, .prev = &zero }, *cur = &zero;
+	struct nobe zero = { .val = 0, .next = &zero, .prev = &zero }, *cur = &zero, *n;
 	long score[elves], lo, hi = 0;
 	memset(score, 0, sizeof score);
-	for (long i = 1; i <= marbs; i++) {
+	for (int i = 1; i <= marbs; i++) {
 		if (i % 23) {
-			struct nobe *n = malloc(sizeof *n);
+			n = malloc(sizeof *n);
 			n->val = i;
 			n->next = cur->next->next;
 			n->prev = cur->next;
 			cur = cur->next->next = cur->next->next->prev = n;
 		} else {
-			struct nobe *n = cur;
+			n = cur;
 			for (int j = 0; j < 7; j++) n = n->prev;
 			n->next->prev = n->prev;
 			cur = n->prev->next = n->next;
