c899b3bf1aca5bccc721bb75f634838008db3032
Ben Blum <bblum@andrew.cmu.edu>
2018-12-09 12:20:15 -0500

    9 hs

---
+++ day09/Day09.hs
@@ -0,0 +1,13 @@
+import Data.Map (fromList,adjust,elems)
+
+ins n (p,a:b:q) = (b:a:p, n:q)
+ins n (p,q) = ins n ([], q ++ reverse p)
+
+del k n (m,(a:b:c:d:e:f:g:p,q)) = (adjust (+(g+n)) (mod n k) m, (p, f:e:d:c:b:a:q))
+del k n (m,(p,q)) = del k n (m, (p ++ reverse q, []))
+
+marble k state n = if mod n 23 == 0 then del k n state else ins n <$> state
+
+solve k n = maximum $ elems $ fst $ foldl (marble k) (fromList $ zip [0..k-1] $ repeat 0, ([0,1],[])) [2..n]
+
+main = print (solve 418 71339, solve 418 7133900)
