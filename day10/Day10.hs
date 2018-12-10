33765eb8cb8cee236b840a67345f34a43db98595
Ben Blum <bblum@andrew.cmu.edu>
2018-12-10 00:55:49 -0500

    10

--- day10/Day10.hs
+++ day10/Day10.hs
@@ -1,5 +1,4 @@
 import Data.List
-import Data.Maybe
 import Control.Arrow
 
 nbr ([x1,y1],_) ([x2,y2],_) = elem x1 [x2-1..x2+1] && elem y1 [y2-1..y2+1]
@@ -12,7 +11,7 @@ parse [_,x,y,_,vx,vy] = ([read x, read y], [read vx, read vy])
 
 output xys = map row [minimum $ map last xys..maximum $ map last xys]
     where row y = map (col y) [minimum $ map head xys..maximum $ map head xys]
-          col y x = if isJust $ find (== [x,y]) xys then '#' else '.'
+          col y x = if elem [x,y] xys then '#' else '.'
 
 main = do input <- map words <$> lines <$> readFile "input.txt"
           let (word, n) = until isword (map update *** (+1)) (map parse input, 0)
