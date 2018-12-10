88f942c7e97689a6eb4be83e09dff70d907cf063
Ben Blum <bblum@andrew.cmu.edu>
2018-12-10 00:53:38 -0500

    10

---
+++ day10/Day10.hs
@@ -0,0 +1,20 @@
+import Data.List
+import Data.Maybe
+import Control.Arrow
+
+nbr ([x1,y1],_) ([x2,y2],_) = elem x1 [x2-1..x2+1] && elem y1 [y2-1..y2+1]
+fnbr ps p = any (nbr p) (ps \\ [p])
+isword (ps,_) = all (fnbr ps) ps
+
+update (xy,v) = (zipWith (+) xy v, v)
+
+parse [_,x,y,_,vx,vy] = ([read x, read y], [read vx, read vy])
+
+output xys = map row [minimum $ map last xys..maximum $ map last xys]
+    where row y = map (col y) [minimum $ map head xys..maximum $ map head xys]
+          col y x = if isJust $ find (== [x,y]) xys then '#' else '.'
+
+main = do input <- map words <$> lines <$> readFile "input.txt"
+          let (word, n) = until isword (map update *** (+1)) (map parse input, 0)
+          mapM print $ output $ map fst word
+          print n
