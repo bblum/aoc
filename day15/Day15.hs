4ceb447c3639b0c60ac1429be350bbf75ba2dde6
Ben Blum <bblum@andrew.cmu.edu>
2018-12-15 23:18:00 -0500

    first run, forgot to implement attack-lowest-hp

--- day15/Day15.hs
+++ day15/Day15.hs
@@ -22,12 +22,13 @@ type Noobs = M.Map Coord Int
 data Board = B { spaces :: S.Set Coord, gobs :: Noobs, elfs :: Noobs, time :: Int } deriving (Ord, Eq)
 
 instance Show Board where
-    show b = intercalate "\n" $ map (flip map [0..maxx+1] . cell) [0..maxy+1]
+    show b = intercalate "\n" $ map (flip concatMap [0..maxx+1] . cell) [0..maxy+1]
         where maxx = maximum $ map snd $ S.elems $ spaces b
               maxy = maximum $ map fst $ S.elems $ spaces b
-              cell y x = if M.member (y,x) $ gobs b then 'G' else
-                         if M.member (y,x) $ elfs b then 'E' else
-                         if S.member (y,x) $ spaces b then '.' else '#'
+              hp f yx = reverse $ take 3 $ (reverse $ show $ f b M.! yx) ++ repeat '0'
+              cell y x = if M.member (y,x) $ gobs b then "G" ++ hp gobs (y,x) else
+                         if M.member (y,x) $ elfs b then "E" ++ hp elfs (y,x) else
+                         if S.member (y,x) $ spaces b then "...." else "####"
 
 parseBoard :: [String] -> Board
 parseBoard input = foldl row (B S.empty M.empty M.empty 0) $ zip [0..] input
@@ -44,26 +45,55 @@ getRace False = gobs
 getRace True  = elfs
 updateRace False f b = b { gobs = f $ gobs b }
 updateRace True  f b = b { elfs = f $ elfs b }
+getEnemy r = getRace $ not r
+updateEnemy r = updateRace $ not r
 
 nbrs :: Coord -> [Coord]
 nbrs (y,x) = [(y-1,x),(y,x-1),(y,x+1),(y+1,x)]
 
-targetSquares :: (Coord, Race) -> State Board [Coord]
-targetSquares = undefined
+open :: Coord -> State Board Bool
+open yx = do space <- S.member yx <$> spaces <$> get
+             nogob <- M.notMember yx <$> gobs <$> get
+             noelf <- M.notMember yx <$> elfs <$> get
+             return $ space && nogob && noelf
 
-path :: Coord -> Coord -> State Board (Maybe Int) -- returns NEGATIVE total number of steps
-path = undefined
+targetSquares :: Coord -> Race -> State Board [Coord]
+targetSquares yx enemies =
+    do enemy_noobs <- M.keys <$> getRace enemies <$> get
+       filterM open $ nub $ concatMap nbrs enemy_noobs
 
-turnMove :: Coord -> Race -> State Board ()
+bfs :: Coord -> Int -> S.Set Coord -> [Coord] -> [Coord] -> State Board (Maybe Int)
+bfs goal n seen [] [] = return Nothing
+bfs goal n seen todo_later [] = bfs goal (n+1) seen [] todo_later
+-- bfs goal n seen todo_later (yx:todo_now) | elem yx $ nbrs goal = return $ Just n
+bfs goal n seen todo_later (yx:todo_now) | yx == goal = return $ Just n
+bfs goal n seen todo_later (yx:todo_now) =
+    do new_todos <- filterM open $ filter (flip S.notMember seen) $ nbrs yx
+       let new_seen = S.union seen $ S.fromList new_todos
+       bfs goal n new_seen (todo_later ++ new_todos) todo_now
+
+-- coord is the first square you should go to in this path; int is the distance thereto
+path :: Coord -> Coord -> State Board (Maybe (Coord, Int))
+path yx goal =
+    do steps <- filterM open $ nbrs yx -- floodfill 4 times, in reading order
+       scores <- mapM (bfs goal 0 S.empty [] . (:[])) steps
+       let steps_scores = catMaybes $ zipWith (\x y -> (x,) <$> y) steps scores
+       if null steps_scores then
+           return Nothing
+       else
+           return $ Just $ minimumBy (comparing snd) steps_scores
+
+turnMove :: Coord -> Race -> State Board Coord
 turnMove yx enemies =
-    do targets <- targetSquares (yx, enemies)
-       paths <- mapM (path yx) targets
-       -- Nothing compares worse than any Just, so gets shortest possible path
-       -- maximumBy breaks ties by preferring later elements, so reverse the read order
-       let (target, path) = maximumBy (comparing snd) $ reverse $ zip targets paths
-       -- XXX
-       undefined
-       return ()
+    do targets <- targetSquares yx enemies
+       paths <- catMaybes <$> mapM (path yx) targets
+       if null paths then
+           return yx
+       else
+           do let (newyx,_) = minimumBy (comparing snd) paths
+              let move m = M.delete yx $ M.insert newyx (m M.! yx) m
+              modify $ updateRace (not enemies) move
+              return newyx
 
 turnAttack :: Race -> Coord -> State Board (Maybe Score)
 turnAttack enemies noob =
@@ -75,21 +105,23 @@ turnAttack enemies noob =
 
 turn :: (Coord, Race) -> State Board (Maybe Score)
 turn (yx, enemies) =
-    do v <- victim
-       when (isNothing v) $ turnMove yx enemies
-       v' <- victim
+    do v <- victim yx
+       yx' <- if isNothing v then Just <$> turnMove yx enemies else return Nothing
+       v' <- case yx' of Just newpos -> victim newpos; Nothing -> return v
        fromMaybe (return Nothing) $ turnAttack enemies <$> v' -- could golf better?
-    where victim = listToMaybe <$> flip filter (nbrs yx) <$> flip M.member <$> getRace enemies <$> get
+    where victim pos = listToMaybe <$> flip filter (nbrs pos) <$> flip M.member <$> getRace enemies <$> get
 
 tick :: State Board (Maybe Outcome)
-tick = do gs <- map (,True)  <$> M.keys <$> gobs <$> get
-          es <- map (,False) <$> M.keys <$> elfs <$> get
-          oldtime <- time <$> get
-          modify $ \b -> b { time = time b + 1 }
-          -- finds the first "Just" final-hp-result among turn results
-          -- if any are Just it means combat should end there & return that total
-          outcome <- join <$> find isJust <$> mapM turn (sortBy (comparing fst) $ gs ++ es)
-          return $ (oldtime *) <$> outcome
+tick =
+    do gs <- map (,True)  <$> M.keys <$> gobs <$> get
+       es <- map (,False) <$> M.keys <$> elfs <$> get
+       oldtime <- time <$> get
+       modify $ \b -> b { time = time b + 1 }
+       -- finds the first "Just" final-hp-result among turn results
+       -- if any are Just it means combat should end there & return that total
+       outcome <- join <$> find isJust <$> mapM turn (sortBy (comparing fst) $ gs ++ es)
+       board <- get
+       traceShow board $ return $ (oldtime *) <$> outcome
 
 simulate :: State Board Outcome
 simulate = liftM2 (flip fromMaybe) tick simulate -- "while !done() tick();" :haskell_think:
