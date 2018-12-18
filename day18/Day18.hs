0d17ce4ead8f2edc14f2621454f4cc704d5c3a99
Ben Blum <bblum@andrew.cmu.edu>
2018-12-18 01:26:15 -0500

    clean 18 even more

--- day18/Day18.hs
+++ day18/Day18.hs
@@ -1,19 +1,16 @@
 import Data.List
 
-update grid (y,x) = rule $ grid !! y !! x
-    where rule '.' = if fnbrs '|' > 2 then '|' else '.'
-          rule '|' = if fnbrs '#' > 2 then '#' else '|'
-          rule '#' = if fnbrs '#' > 0 && fnbrs '|' > 0 then '#' else '.'
-          fnbrs c = sum [ 1 | w <- intersect [y-1..y+1] [0..length grid - 1],
-                              z <- intersect [x-1..x+1] [0..length (head grid) - 1],
-                              (w,z) /= (y,x) && (grid !! w !! z) == c ]
-
-minute grid = map (map (update grid)) $ coords grid
-
-coords grid = map (flip map [0..length (head grid) - 1] . (,)) [0..length grid - 1]
+tick grid = [ [ update (y,x) | (_,x) <- zip row [0..] ] | (row,y) <- zip grid [0..] ]
+    where update (y,x) = rule $ grid !! y !! x
+              where rule '.' = if fnbrs '|' > 2 then '|' else '.'
+                    rule '|' = if fnbrs '#' > 2 then '#' else '|'
+                    rule '#' = if fnbrs '#' > 0 && fnbrs '|' > 0 then '#' else '.'
+                    fnbrs c = sum [ 1 | w <- intersect [y-1..y+1] [0..length grid - 1],
+                                        z <- intersect [x-1..x+1] [0..length (head grid) - 1],
+                                        (w,z) /= (y,x), c == grid !! w !! z ]
 
 score grid = count '#' * count '|' where count x = sum [ 1 | r <- grid, c <- r, c == x ]
 
 main = do input <- lines <$> readFile "input.txt"
-          print $ score $ iterate minute input !! 10
-          print $ score $ iterate minute input !! 2400
+          print $ score $ iterate tick input !! 10
+          print $ score $ iterate tick input !! 2400
