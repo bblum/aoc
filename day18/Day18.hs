5b50f75a5c9b477a1d462e44f20679067330e202
Ben Blum <bblum@andrew.cmu.edu>
2018-12-18 11:40:26 -0500

    better fn names for 18

--- day18/Day18.hs
+++ day18/Day18.hs
@@ -1,15 +1,15 @@
 import Data.List
 
-tick grid = [ [ rule (y,x) c | (c,x) <- zip row [0..] ] | (row,y) <- zip grid [0..] ]
-    where rule yx '.' = if fnbrs yx '|' > 2 then '|' else '.'
-          rule yx '|' = if fnbrs yx '#' > 2 then '#' else '|'
-          rule yx '#' = if fnbrs yx '#' > 0 && fnbrs yx '|' > 0 then '#' else '.'
-          fnbrs (y,x) c = sum [ 1 | w <- intersect [y-1..y+1] [0..length grid - 1],
-                                    z <- intersect [x-1..x+1] [0..length (head grid) - 1],
-                                    (w,z) /= (y,x), c == grid !! w !! z ]
+update input = [ [ rule (y,x) c | (c,x) <- zip row [0..] ] | (row,y) <- zip input [0..] ]
+    where rule yx '.' = if adj yx '|' > 2 then '|' else '.'
+          rule yx '|' = if adj yx '#' > 2 then '#' else '|'
+          rule yx '#' = if adj yx '#' > 0 && adj yx '|' > 0 then '#' else '.'
+          adj (y,x) c = sum [ 1 | w <- intersect [y-1..y+1] [0..length input - 1],
+                                  z <- intersect [x-1..x+1] [0..length (head input) - 1],
+                                  (w,z) /= (y,x), c == input !! w !! z ]
 
-score grid = count '#' * count '|' where count x = sum [ 1 | r <- grid, c <- r, c == x ]
+score input = count '#' * count '|' where count x = sum [ 1 | r <- input, c <- r, c == x ]
 
 main = do input <- lines <$> readFile "input.txt"
-          print $ score $ iterate tick input !! 10
-          print $ score $ iterate tick input !! 2400
+          print $ score $ iterate update input !! 10
+          print $ score $ iterate update input !! 2400
