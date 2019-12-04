3106ea4dd2cb4e46f4d8381346cd036ab6a01daf
ben blum <bblum0@gmail.com>
2019-12-04 00:42:28 -0500

    clean up day 4

--- day04/Day04.hs
+++ day04/Day04.hs
@@ -1,11 +1,11 @@
 import Data.List
 
-input = map show [130254..678275]
-
 part1 x = any (uncurry (==)) $ zip x $ tail x
 part2 x = any ((== 2) . length) $ group x
 
-ispass match_fn x = match_fn x && (all (uncurry (<=)) $ zip x $ tail x)
+ascending x = all (uncurry (<=)) $ zip x $ tail x
 
-main = do print $ length $ filter (ispass part1) input
-          print $ length $ filter (ispass part2) input
+candidates = filter ascending $ map show [130254..678275]
+
+main = do print $ length $ filter part1 candidates
+          print $ length $ filter part2 candidates
