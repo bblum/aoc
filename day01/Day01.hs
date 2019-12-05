9bac8ae6148251fa099015a4a764f7d8f1425d71
ben blum <bblum0@gmail.com>
2019-12-05 02:25:33 -0500

    remove unused imports

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -1,14 +1,3 @@
-{-# LANGUAGE FlexibleContexts, TupleSections, MonadComprehensions #-}
-import qualified Data.Map as M
-import qualified Data.Set as S
-import Data.List
-import Data.Maybe
-import Data.Char
-import Data.Ord
-import Control.Monad.State
-import Control.Arrow
-import Debug.Trace
-
 fuel x = if f == 0 then 0 else f + fuel f
     where f = max 0 $ (div x 3) - 2
 
