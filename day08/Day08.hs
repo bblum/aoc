dc9a3cf0848d29da3b4b5948954a576699beebc3
ben blum <bblum0@gmail.com>
2019-12-08 11:47:25 -0500

    count

--- day08/Day08.hs
+++ day08/Day08.hs
@@ -7,8 +7,9 @@ width = 25
 height = 6
 size = width * height
 
-part1 input = length (filter (=='1') minzeroes) * length (filter (=='2') minzeroes)
-    where minzeroes = minimumBy (comparing (length . filter (=='0'))) input
+part1 input = count '1' minzeroes * count '2' minzeroes
+    where minzeroes = minimumBy (comparing (count '0')) input
+          count c = length . filter (== c)
 
 part2 input = chunksOf width $ map (fromJust . find (/='2')) $ transpose input
 
