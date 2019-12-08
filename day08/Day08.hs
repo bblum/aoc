0738be96a2ff169bf86ca9e166e4af433535cb5d
ben blum <bblum0@gmail.com>
2019-12-08 09:23:09 -0500

    transpose

--- day08/Day08.hs
+++ day08/Day08.hs
@@ -1,5 +1,6 @@
 import Data.List
 import Data.List.Split
+import Data.Maybe
 import Data.Ord
 
 width = 25
@@ -9,9 +10,7 @@ size = width * height
 part1 input = length (filter (=='1') minzeroes) * length (filter (=='2') minzeroes)
     where minzeroes = minimumBy (comparing (length . filter (=='0'))) input
 
-part2 input = chunksOf width $ foldl (zipWith merge) (repeat '2') input
-    where merge '2' y = y
-          merge x y = x
+part2 input = chunksOf width $ map (fromJust . find (/='2')) $ transpose input
 
 main = do input <- chunksOf size <$> head <$> lines <$> readFile "input.txt"
           print $ part1 input
