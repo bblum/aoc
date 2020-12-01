00de02e243f518c61ffe0bc96dc756ca966a192e
ben blum <bblum@dropbox.com>
2020-12-01 14:51:24 -0800

    golf golf gofl

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -1,16 +1,3 @@
-import Data.List
-
-find2 target (i:is) =
-    case find (== target-i) is of
-        Just ans -> Just $ ans * i
-        Nothing -> find2 target is
-find2 target [] = Nothing
-
-find3 (i:is) =
-    case find2 (2020-i) is of
-        Just ans -> ans * i
-        Nothing -> find3 is
-
 main = do input <- map read <$> lines <$> readFile "input.txt"
-          print $ find2 2020 input
-          print $ find3 input
+          print [ x * y | x <- input, y <- input, x + y == 2020, x < y ]
+          print [ x * y * z | x <- input, y <- input, z <- input, x + y + z == 2020, x < y, y < z ]
