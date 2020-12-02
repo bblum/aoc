e75629a28949885f0e46fadcd5e8eed62a338db5
ben blum <bblum0@gmail.com>
2020-12-02 00:29:04 -0500

    02

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -1,12 +1,14 @@
 import Data.List.Split
 
+count f = length . filter f
+
 parse [ij, [c, ':'], password] = (map read $ splitOn "-" ij, c, password)
 
-legal1 ([i,j], c, password) = count >= i && count <= j
-    where count = length $ filter (== c) password
+legal1 ([i,j], c, password) = n >= i && n <= j
+    where n = count (== c) password
 
-legal2 (ij, c, password) = length (filter id (map (\x -> password !! (x-1) == c) ij)) == 1
+legal2 (ij, c, password) = count id (map (\x -> password !! (x-1) == c) ij) == 1
 
 main = do input <- map (parse . words) <$> lines <$> readFile "input.txt"
-          print $ length $ filter legal1 input
-          print $ length $ filter legal2 input
+          print $ count legal1 input
+          print $ count legal2 input
