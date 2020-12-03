e49283b8dc6c170ed77a3cbfe13f26c39bc8ba2c
ben blum <bblum0@gmail.com>
2020-12-03 00:24:32 -0500

    golf3

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -1,38 +1,8 @@
-{-# LANGUAGE FlexibleContexts, TupleSections, MonadComprehensions #-}
-import qualified Data.Map as M
-import qualified Data.Set as S
-import Data.List
 import Data.List.Split
-import Data.Maybe
-import Data.Char
-import Data.Ord
-import Control.Monad.State
-import Control.Arrow
-import Debug.Trace
 
-count f = length . filter f
-
-foo (line, n) = line !! index
-    where index = (n * 3) `mod` (length line)
-
-foo2 (line, n) = line !! index
-    where index = (n * 1) `mod` (length line)
-foo3 (line, n) = line !! index
-    where index = (n * 5) `mod` (length line)
-foo4 (line, n) = line !! index
-    where index = (n * 7) `mod` (length line)
-
-solve foo input =
-    let width = length (input !! 0)
-        noobs = map foo $ zip input [0..]
-    in count (== '#') noobs
-
-fux (i:j:is) = i:(fux is)
-fux (i:[]) = []
-fux [] = []
-
-solve2 input = product (map (\f -> solve f input) [foo, foo2, foo3, foo4]) * five
-    where five = solve foo2 $ fux input
+solve input (dx,dy) = length $ filter tree $ zip [0..] $ map head $ chunksOf dy input
+    where tree (y, line) = line !! (y * dx `mod` length line) == '#'
 
 main = do input <- lines <$> readFile "input.txt"
-          print $ solve2 input
+          print $ solve input (3,1)
+          print $ product $ map (solve input) [(1,1),(3,1),(5,1),(7,1),(1,2)]
