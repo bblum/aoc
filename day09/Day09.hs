e8e30c952ab8daa7337f155ebd4a3da5c23ff674
ben blum <bblum0@gmail.com>
2020-12-09 00:27:48 -0500

    golf9

--- day09/Day09.hs
+++ day09/Day09.hs
@@ -1,11 +1,10 @@
 import Data.List
 import Data.Maybe
 
-invalid input (i,n) = and [ (input !! a) + (input !! b) /= n | a <- noobs, b <- noobs, a /= b ]
-    where noobs = [i-25..i-1]
+invalid input (i,n) = and [ input !! a + input !! b /= n | a <- [i-25..i], b <- [i-25..i], a /= b ]
 
 solve input (i,n) = minimum range + maximum range
-    where range = head [ range | start <- [0..i], end <- [start+1..i], let range = drop start $ take end input, sum range == n]
+    where range = head [ range | start <- [0..i], end <- [start+1..i], let range = drop start $ take end input, sum range == n ]
 
 main = do input <- map read <$> lines <$> readFile "input.txt"
           let solution = fromJust $ find (invalid input) $ drop 25 $ zip [0..] input
