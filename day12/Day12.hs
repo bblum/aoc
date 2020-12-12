0645916ec552a3de057d37a92d42532e81e688a7
ben blum <bblum0@gmail.com>
2020-12-12 00:40:32 -0500

    golf12

---
+++ day12/Day12.hs
@@ -0,0 +1,33 @@
+parse (c:rest) = (c, read rest)
+
+ship ([y,x],facing) ('N', noob) = ([y-noob,x],facing)
+ship ([y,x],facing) ('E', noob) = ([y,x+noob],facing)
+ship ([y,x],facing) ('S', noob) = ([y+noob,x],facing)
+ship ([y,x],facing) ('W', noob) = ([y,x-noob],facing)
+ship (yx,facing) ('L', noob) = (yx, (facing + 4 - (div noob 90)) `mod` 4)
+ship (yx,facing) ('R', noob) = (yx, (facing + (div noob 90)) `mod` 4)
+ship (yx,facing) ('F', noob) = ship (yx,facing) (dir facing, noob)
+    where dir 0 = 'N'
+          dir 1 = 'E'
+          dir 2 = 'S'
+          dir 3 = 'W'
+
+ship2 (yx, (wayy,wayx)) ('N', noob) = (yx, (wayy-noob, wayx))
+ship2 (yx, (wayy,wayx)) ('E', noob) = (yx, (wayy, wayx+noob))
+ship2 (yx, (wayy,wayx)) ('S', noob) = (yx, (wayy+noob, wayx))
+ship2 (yx, (wayy,wayx)) ('W', noob) = (yx, (wayy, wayx-noob))
+ship2 ([y,x], (wayy,wayx)) ('F', noob) = ([y+dy,x+dx], (wayy+dy,wayx+dx))
+    where (dy,dx) = (noob * (wayy-y), noob * (wayx-x))
+ship2 ([y,x], (wayy,wayx)) (lr, noob) = ([y,x], turn lr noob)
+    where (dy,dx) = (y-wayy, x-wayx)
+          turn 'L' 90 = (y+dx, x-dy)
+          turn 'L' 180 = (y+dy, x+dx)
+          turn 'L' 270 = (y-dx, x+dy)
+          turn 'R' noob = turn 'L' $ 360-noob
+
+solve f start = sum . map abs . fst . foldl f start
+
+main = do input <- map parse <$> lines <$> readFile "input.txt"
+          print $ solve ship ([0,0],1) input
+          print $ solve ship2 ([0,0],(-1,10)) input
+
--- setup-day.sh
+++ setup-day.sh
@@ -10,4 +10,5 @@ fi
 mkdir day$1 || die "couldnt make dir"
 #cp template-c/* day$1 || die "couldnt copy contents"
 cp template-hs/Template.hs day$1/Day$1.hs || die "couldnt copy hs"
+touch day$1/input.txt || die "couldnt make input txt"
 cd day$1 || die "wut"
