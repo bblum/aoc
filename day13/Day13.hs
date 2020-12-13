9dfe784f02d1eb35546bf07b71e3d665205fd30b
ben blum <bblum0@gmail.com>
2020-12-13 16:21:27 -0500

    13 - not coprime sample

--- day13/Day13.hs
+++ day13/Day13.hs
@@ -2,16 +2,23 @@ import Data.List
 import Data.Ord
 
 start = 1013728
-input = [23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,733,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,449,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37] where x = 0
+x = 0
+input = [23,x,x,x,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,733,x,x,x,x,x,x,x,x,x,x,x,x,13,17,x,x,x,x,19,x,x,x,x,x,x,x,x,x,29,x,449,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,37]
 
-part1 = (time - start) * bus
+sample1 = [7,13,x,x,59,x,31,19]
+sample2 = [7,13,3,4,59,18,31,19]
+
+part1 input = (time - start) * bus
     where buses = filter (>0) input
           bustime b = (1 + div start b) * b
           (bus,time) = minimumBy (comparing snd) $ zip buses $ map bustime buses
 
-part2 = fst $ foldl crt (0,1) $ filter ((>0) . fst) $ zip input [0..]
-    where crt (base,step) (bus,offset) = (head $ filter ok [base,base+step..], bus * step)
+part2 input = fst $ foldl crt (0,1) $ filter ((>0) . fst) $ zip input [0..]
+    where crt (base,step) (bus,offset) = (newbase,next-newbase)
               where ok t = mod t bus == mod (bus - offset) bus
+                    (newbase:next:_) = filter ok [base,base+step..]
 
-main = do print part1
-          print part2
+main = do print $ part1 input
+          print $ part2 sample1 -- 1068781
+          print $ part2 sample2 -- 1068781
+          print $ part2 input
