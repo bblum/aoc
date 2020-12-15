8909f47648855c10ae01cdc8c74d42c90363cfbc
ben blum <bblum0@gmail.com>
2020-12-15 12:47:39 -0500

    golf15

--- day15/Day15.hs
+++ day15/Day15.hs
@@ -4,8 +4,7 @@ input = [1,0,18,10,19,6]
 
 step (n, (t, seen)) = (maybe 0 (t-) (M.lookup n seen), (t+1, M.insert n t seen))
 
-solve n = fst $ iterate step (i, (length rest, M.fromList $ zip (reverse rest) [0..])) !! (n - length input)
-    where i:rest = reverse input
+solve n = fst $ iterate step (0, (length input, M.fromList $ zip input [0..])) !! (n - length input - 1)
 
 main = do print $ solve 2020
           print $ solve 30000000
