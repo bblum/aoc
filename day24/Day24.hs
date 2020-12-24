c896bb0d3faeb1312c6c35de63b585a02d00bbb0
ben blum <bblum0@gmail.com>
2020-12-24 01:09:28 -0500

    24

--- day24/Day24.hs
+++ day24/Day24.hs
@@ -1,57 +1,27 @@
-{-# LANGUAGE FlexibleContexts, TupleSections, MonadComprehensions #-}
 import qualified Data.Map as M
-import qualified Data.Set as S
-import Data.List
-import Data.List.Split
-import Data.Either
-import Data.Maybe
-import Data.Char
-import Data.Ord
-import Control.Monad.State
-import Control.Arrow
-import Debug.Trace
 
-parse [] = []
-parse [x] = [[x]]
-parse ('n':'e':rest) = "ne":parse rest
-parse ('n':'w':rest) = "nw":parse rest
-parse ('s':'e':rest) = "se":parse rest
-parse ('s':'w':rest) = "sw":parse rest
+parse (ns:ew:rest) | elem ns "ns" && elem ew "ew" = [ns,ew]:parse rest
 parse (x:rest) = [x]:parse rest
+parse [] = []
 
 fnbrs6 (y,x) = [(y,x-2),(y,x+2),(y-1,x-1),(y-1,x+1),(y+1,x-1),(y+1,x+1)]
 
-coord yx [] = yx
-coord (y,x) ("n":rest) = error "hex"
-coord (y,x) ("e":rest) = coord (y,x+2) rest
-coord (y,x) ("s":rest) = error "hex"
-coord (y,x) ("w":rest) = coord (y,x-2) rest
-coord (y,x) ("ne":rest) = coord (y-1,x+1) rest
-coord (y,x) ("nw":rest) = coord (y-1,x-1) rest
-coord (y,x) ("se":rest) = coord (y+1,x+1) rest
-coord (y,x) ("sw":rest) = coord (y+1,x-1) rest
+coord yx dir = (M.! dir) $ M.fromList $ zip ["w","e","nw","ne","sw","se"] $ fnbrs6 yx
 
-lookupdef m yx = case M.lookup yx m of Just x -> x; Nothing -> False
+query m yx = maybe False id $ M.lookup yx m
 
 count = length . filter id
 
-insertify m yx = M.insert yx (not $ lookupdef m yx) m
+toggle m yx = M.insert yx (not $ query m yx) m
 
-step m yx = ans
-    where live_nbrs = count $ map (lookupdef m) $ fnbrs6 yx
-          self = lookupdef m yx
-          ans = case (self, live_nbrs) of
-                    (True, n) | n == 0 || n > 2 -> False
-                    (False, n) | n == 2 -> True
-                    _ -> self
-
-stepall :: Noob -> Noob
-stepall m = M.fromList [((y,x), step m (y,x)) | y <- [miny-1..maxy+1], x <- [minx-2..maxx+2]]
+run m = M.fromList [((y,x), cell (y,x)) | y <- range ys 1, x <- range xs 2, mod (x+y) 2 == 0]
     where (ys,xs) = unzip $ M.keys m
-          (maxy,miny) = (maximum ys, minimum ys)
-          (maxx,minx) = (maximum xs, minimum xs)
+          range l step = [minimum l - step .. maximum l + step]
+          cell yx = case count $ map (query m) $ yx:fnbrs6 yx of
+              2 -> True
+              x | elem x [0,3] -> query m yx
+              _ -> False
 
-main = do input <- map parse <$> lines <$> readFile "input.txt"
-          let start = foldl insertify M.empty $ map (coord (0,0)) input
-          print $ count $ M.elems $ start
-          print $ count $ M.elems $ iterate stepall start !! 100
+main = do state <- foldl toggle M.empty <$> map (foldl coord (0,0) . parse) <$> lines <$> readFile "input.txt"
+          print $ count $ M.elems state
+          print $ count $ M.elems $ iterate run state !! 100
