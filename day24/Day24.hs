0d07e327cfcb2a30291b1bf86713c046848ea299
ben blum <bblum0@gmail.com>
2020-12-24 15:16:15 -0500

    24-set

--- day24/Day24.hs
+++ day24/Day24.hs
@@ -1,26 +1,24 @@
-import qualified Data.Map as M
-
-count = length . filter id
+import qualified Data.Set as S
+import Data.Maybe
 
 fnbrs6 (y,x) = [(y,x-2),(y,x+2),(y-1,x-1),(y-1,x+1),(y+1,x-1),(y+1,x+1)]
 
-parse (ns:ew:rest) | elem ns "ns" && elem ew "ew" = [ns,ew]:parse rest
+parse (ns:ew:rest) | elem ns "ns" = [ns,ew]:parse rest
 parse (x:rest) = [x]:parse rest
 parse [] = []
 
-coord yx dir = (M.! dir) $ M.fromList $ zip ["w","e","nw","ne","sw","se"] $ fnbrs6 yx
+coord yx dir = fromJust $ lookup dir $ zip ["w","e","nw","ne","sw","se"] $ fnbrs6 yx
 
-query m yx = maybe False id $ M.lookup yx m
-toggle m yx = M.insert yx (not $ query m yx) m
+toggle s yx = if S.member yx s then S.delete yx s else S.insert yx s
 
-run m = M.fromList [((y,x), cell (y,x)) | y <- range ys 1, x <- range xs 2, mod (x+y) 2 == 0]
-    where (ys,xs) = unzip $ M.keys m
+run s = S.fromList [(y,x) | y <- range ys 1, x <- range xs 2, mod (x+y) 2 == 0, cell (y,x)]
+    where (ys,xs) = unzip $ S.elems s
           range l step = [minimum l - step .. maximum l + step]
-          cell yx = case count $ map (query m) $ yx:fnbrs6 yx of
+          cell yx = case length $ filter (flip S.member s) $ yx:fnbrs6 yx of
               2 -> True
-              x | elem x [0,3] -> query m yx
+              x | elem x [0,3] -> S.member yx s
               _ -> False
 
-main = do state <- foldl toggle M.empty <$> map (foldl coord (0,0) . parse) <$> lines <$> readFile "input.txt"
-          print $ count $ M.elems state
-          print $ count $ M.elems $ iterate run state !! 100
+main = do state <- foldl toggle S.empty <$> map (foldl coord (0,0) . parse) <$> lines <$> readFile "input.txt"
+          print $ S.size state
+          print $ S.size $ iterate run state !! 100
