4eab7befd034bf387acacfd5c1211d33b6664e72
ben blum <bblum0@gmail.com>
2021-12-06 10:07:01 -0500

    golf3

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -8,8 +8,8 @@ median input = (transpose $ map sort $ transpose input) !! div (length input) 2
 part1 input = parse '0' m * parse '1' m where m = median input
 
 p2 _ _ [w] = w
-p2 f i input = p2 f (i+1) $ filter (f (median input !! i) . (!! i)) input
+p2 e i input = p2 e (i+1) $ filter (e (median input !! i) . (!! i)) input
 
-part2 input = parse '0' (p2 (==) 0 input) * parse '0' (p2 (/=) 0 input)
+part2 input = rate (==) * rate (/=) where rate e = parse '0' $ p2 e 0 input
 
 main = print =<< (part1 &&& part2) <$> lines <$> readFile "input.txt"
