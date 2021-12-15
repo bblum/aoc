77580a33e54a0e793dd8af33cd6cb492052017c9
ben blum <bblum0@gmail.com>
2021-12-14 22:38:44 -0500

    golf-some

--- day14/Day14.hs
+++ day14/Day14.hs
@@ -11,51 +11,24 @@ import Control.Monad.State
 import Control.Arrow
 import Debug.Trace
 
-fnbrs8 (y,x) = [(y2,x2) | y2 <- [y-1..y+1], x2 <- [x-1..x+1], (y2,x2) /= (y,x)]
-fnbrs4 (y,x) = [(y-1,x), (y+1,x), (y,x-1), (y,x+1)]
-
-parse = f . words where f [a,b] = (a,b)
-
-step :: [(String, String)] -> String -> String
-step rules polymer = (concat $ zipWith insert polymer $ tail polymer) ++ [last polymer]
-    where insert a b = a:(fromJust $ lookup [a,b] rules)
-
-part1 polymer = maximum quantities - minimum quantities
-    where quantities = map length $ group $ sort polymer
-
--- part 2:
--- memoized it with a map of ([a,b],depth) -> (map of frequencies)
--- append the maps by adding them at each level
-
-type FrequencyMap = M.Map Char Int
-type DepthMap = M.Map ((Char,Char),Int) FrequencyMap
-
-addMaps = foldl $ M.unionWith (+)
+add = foldl $ M.unionWith (+)
 
 generate rules m = M.fromList $ map recurse rules
-    where recurse ([a,b],[c]) = ((a,b), addMaps (M.singleton c 1) [m M.! (a,c), m M.! (c,b)])
+    where recurse [[a,b],[c]] = ((a,b), add (M.singleton c 1) [m M.! (a,c), m M.! (c,b)])
 
 -- TODO golf this to use 'map $ const M.empty' lmao
 -- means changing key type back to list
-start ([a,b],_) = ((a,b), M.empty)
+start [[a,b],_] = ((a,b), M.empty)
 
-solve ans = maximum ts - minimum ts where ts = map snd $ totals ans
-totals ans = M.toList $ addMaps templateMap $ map (ans M.!) $ zip template $ tail template
-    where templateMap = M.fromList $ map (head &&& length) $ group $ sort template
+solve ans = maximum totals - minimum totals
+    where totals = map snd $ M.toList $ add templateMap $ map (ans M.!) $ zip template $ tail template
+          templateMap = M.fromList $ map (head &&& length) $ group $ sort template
 
-part n input = iterate (generate input) (M.fromList $ map start input) !! n
+part n rules = solve $ iterate (generate rules) (M.fromList $ map start rules) !! n
 
-main = do input <- map parse <$> lines <$> readFile "input.txt"
-          print $ part1 $ iterate (step input) template !! 10
-          -- let ans = iterate (generate input) (M.fromList $ map start input) !! 10
-          -- mapM print $ M.toList ans
-          -- print $ solve ans
-          print template
-          print $ step input template
-          print $ step input $ step input template
-          -- mapM print $ M.toList $ part 1 input
-          print $ totals $ part 1 input
-          print $ solve $ part 40 input
+main = do rules <- map words <$> lines <$> readFile "input.txt"
+          print $ part 10 rules
+          print $ part 40 rules
 
 -- template = "NNCB"
 template = "PHOSBSKBBBFSPPPCCCHN"
