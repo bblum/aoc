f360a7aeaa24c3a34c539b2694b536be187a3fa2
ben blum <bblum0@gmail.com>
2022-12-06 16:37:27 -0500

    golf3

--- day03/Day03.hs
+++ day03/Day03.hs
@@ -1,16 +1,12 @@
 import qualified Data.Set as S
-import Data.List
 import Data.List.Split
 
 priority c = if x > 97 then x - 96 else x - 64 + 26 where x = fromEnum c
 
--- TODO write with splitAt
-split sack = (take n sack, drop n sack) where n = length sack `div` 2
+halve sack = [take n sack, drop n sack] where n = length sack `div` 2
 
-part1 (l,r) = priority x where [x] = S.toList $ S.intersection (S.fromList l) (S.fromList r)
-
-part2 sacks = priority x where [x] = S.toList $ foldl1 S.intersection $ map S.fromList sacks
+solve = priority . head . S.toList . foldl1 S.intersection . map S.fromList
 
 main = do input <- lines <$> readFile "input.txt"
-          print $ sum $ map (part1 . split) input
-          print $ sum $ map part2 $ chunksOf 3 input
+          print $ sum $ map solve $ map halve input
+          print $ sum $ map solve $ chunksOf 3 input
