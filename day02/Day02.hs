9ca46d4d67fa981c7937cb8ba33234c6d6279e11
ben blum <bblum0@gmail.com>
2022-12-06 15:17:50 -0500

    day 2 the smrt way

--- day02/Day02.hs
+++ day02/Day02.hs
@@ -1,29 +1,8 @@
-shape [_,"X"] = 1
-shape [_,"Y"] = 2
-shape [_,"Z"] = 3
+parse [[a],[x]] = (fromEnum a - 65, fromEnum x - 88) 
 
-outcome ["C","X"] = 6
-outcome ["A","Y"] = 6
-outcome ["B","Z"] = 6
-outcome ["A","X"] = 3
-outcome ["B","Y"] = 3
-outcome ["C","Z"] = 3
-outcome _ = 0
+part1 (a,x) = x+1 + (3 * mod (4+x-a) 3)
+part2 (a,x) = 3*x + (1 + mod (2+a+x) 3)
 
-outcome2 [_,"X"] = 0
-outcome2 [_,"Y"] = 3
-outcome2 [_,"Z"] = 6
-
-shape2 ["A","X"] = 3
-shape2 ["A","Y"] = 1
-shape2 ["A","Z"] = 2
-shape2 ["B","X"] = 1
-shape2 ["B","Y"] = 2
-shape2 ["B","Z"] = 3
-shape2 ["C","X"] = 2
-shape2 ["C","Y"] = 3
-shape2 ["C","Z"] = 1
-
-main = do input <- map words <$> lines <$> readFile "input.txt"
-          print $ sum $ map shape input ++ map outcome input
-          print $ sum $ map shape2 input ++ map outcome2 input
+main = do input <- map (parse . words) <$> lines <$> readFile "input.txt"
+          print $ sum $ map part1 input
+          print $ sum $ map part2 input
